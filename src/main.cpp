196250acf9e8bc80b374a3c7d154fc86d342eee6
JL <j1111011@163.com>
2013-05-02 13:12:56 +0800

    remove textcodec call

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,6 @@
 
 int main(int argc, char *argv[])
 {
-    QTextCodec *codec = QTextCodec::codecForName("GB2312");
-
-    QTextCodec::setCodecForLocale(codec);
-#if QT_VERSION <= 4
-    QTextCodec::setCodecForCStrings(codec);
-
-    QTextCodec::setCodecForTr(codec);
-//#else
-#endif
     QApplication a(argc, argv);
     QTranslator t;
     t.load("socket-test.cn.qm");
